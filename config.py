f1f46a63f55e600b4f020e8832fdae5ade5f037b
Sachin Ravindran <75080152+sachinravindran@users.noreply.github.com>
2026-07-06 22:06:15 +0530

    Add initial configuration settings in config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+mongouri     = "mongodb://mongodb:27017/"
+masterdb     = "airnotifier"
+pemdir       = "/var/airnotifier/pemdir"
+port         = 8801
+https        = False
+passwordsalt = "REPLACE_ME"
+cookiesecret = "REPLACE_ME"
