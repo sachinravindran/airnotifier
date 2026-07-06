f417bf1147ed210b13cf1bd3dd7199f2b5f4f1a9
Sachin Ravindran <75080152+sachinravindran@users.noreply.github.com>
2026-07-06 22:07:48 +0530

    Update sensitive configuration values in config.py
    Updated passwordsalt and cookiesecret values for security.

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-mongouri     = "mongodb://mongodb:27017/"
-masterdb     = "airnotifier"
-pemdir       = "/var/airnotifier/pemdir"
-port         = 8801
-https        = False
-passwordsalt = "REPLACE_ME"
-cookiesecret = "REPLACE_ME"
+mongouri = "mongodb://mongodb:27017/"
+masterdb = "airnotifier"
+pemdir = "/var/airnotifier/pemdir"
+port = 8801
+https = False
+passwordsalt = "d2o0n1g2s0h3e1n1gdcvtp"
+cookiesecret = "dcvtpgp23cj64qy6m3f6mt24d"
